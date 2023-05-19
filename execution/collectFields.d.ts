18ab7055c52a9ffcd9a32cf441fbca631f697730
GitHub Action Script <please@open.issue>
2023-05-19 12:10:20 +0000

    Deploy  to 'npm' branch

--- execution/collectFields.d.ts
+++ execution/collectFields.d.ts
@@ -1,11 +1,21 @@
-import type { ObjMap } from '../jsutils/ObjMap';
+import type { ObjMap } from '../jsutils/ObjMap.js';
 import type {
   FieldNode,
   FragmentDefinitionNode,
-  SelectionSetNode,
-} from '../language/ast';
-import type { GraphQLObjectType } from '../type/definition';
-import type { GraphQLSchema } from '../type/schema';
+  OperationDefinitionNode,
+} from '../language/ast.js';
+import type { GraphQLObjectType } from '../type/definition.js';
+import type { GraphQLSchema } from '../type/schema.js';
+export type FieldGroup = ReadonlyArray<FieldNode>;
+export type GroupedFieldSet = Map<string, FieldGroup>;
+export interface PatchFields {
+  label: string | undefined;
+  groupedFieldSet: GroupedFieldSet;
+}
+export interface FieldsAndPatches {
+  groupedFieldSet: GroupedFieldSet;
+  patches: Array<PatchFields>;
+}
 /**
  * Given a selectionSet, collects all of the fields and returns them.
  *
@@ -22,8 +32,8 @@ export declare function collectFields(
     [variable: string]: unknown;
   },
   runtimeType: GraphQLObjectType,
-  selectionSet: SelectionSetNode,
-): Map<string, ReadonlyArray<FieldNode>>;
+  operation: OperationDefinitionNode,
+): FieldsAndPatches;
 /**
  * Given an array of field nodes, collects all of the subfields of the passed
  * in fields, and returns them at the end.
@@ -40,6 +50,7 @@ export declare function collectSubfields(
   variableValues: {
     [variable: string]: unknown;
   },
+  operation: OperationDefinitionNode,
   returnType: GraphQLObjectType,
-  fieldNodes: ReadonlyArray<FieldNode>,
-): Map<string, ReadonlyArray<FieldNode>>;
+  fieldGroup: FieldGroup,
+): FieldsAndPatches;
